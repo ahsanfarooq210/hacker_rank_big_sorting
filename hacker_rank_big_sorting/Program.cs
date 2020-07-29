a83f46d366aa94b4f9aebbce7cf63c358f336487
Ahsan <ahsanfarooq210@gmail.com>
2020-07-29 16:50:51 +0500

    Initial commit

---
+++ hacker_rank_big_sorting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace hacker_rank_big_sorting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] a = new string[n];
+            
+            for (int i = 0; i < n; ++i) a[i] = Console.ReadLine();
+
+            Array.Sort(a, (s1, s2) =>
+            {
+                int l = s1.Length - s2.Length;
+                return (l != 0) ? l : string.Compare(s1, s2, StringComparison.OrdinalIgnoreCase);
+            });
+
+            for (int i = 0; i < n; ++i) Console.WriteLine(a[i]);
+        }
+    }
+}
